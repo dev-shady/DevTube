a07ee417b7ad843ec51eb160278036d8e1f7b060
Amit Kumar <itsdevshady@gmail.com>
2026-05-22 23:18:05 +0530

    Final: Application entry point and theme resources

---
+++ app/src/main/java/com/devshady/devtube/DevTubeApp.kt
@@ -0,0 +1,7 @@
+package com.devshady.devtube
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DevTubeApp : Application()
---
+++ app/src/main/java/com/devshady/devtube/ui/theme/Color.kt
@@ -0,0 +1,59 @@
+package com.devshady.devtube.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Vibrant Material 3 Color Palette for DevTube
+val PrimaryLight = Color(0xFF984061)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFFFD9E2)
+val OnPrimaryContainerLight = Color(0xFF3E001D)
+
+val SecondaryLight = Color(0xFF7D5260)
+val OnSecondaryLight = Color(0xFFFFFFFF)
+val SecondaryContainerLight = Color(0xFFFFD9E2)
+val OnSecondaryContainerLight = Color(0xFF31111D)
+
+val TertiaryLight = Color(0xFF815233)
+val OnTertiaryLight = Color(0xFFFFFFFF)
+val TertiaryContainerLight = Color(0xFFFFDBCA)
+val OnTertiaryContainerLight = Color(0xFF321200)
+
+val ErrorLight = Color(0xFFBA1A1A)
+val OnErrorLight = Color(0xFFFFFFFF)
+val ErrorContainerLight = Color(0xFFFFDAD6)
+val OnErrorContainerLight = Color(0xFF410002)
+
+val BackgroundLight = Color(0xFFFFFBFF)
+val OnBackgroundLight = Color(0xFF201A1B)
+val SurfaceLight = Color(0xFFFFFBFF)
+val OnSurfaceLight = Color(0xFF201A1B)
+val SurfaceVariantLight = Color(0xFFF2DDE1)
+val OnSurfaceVariantLight = Color(0xFF514347)
+
+// Dark Palette
+val PrimaryDark = Color(0xFFFFB1C8)
+val OnPrimaryDark = Color(0xFF5E1133)
+val PrimaryContainerDark = Color(0xFF7B2949)
+val OnPrimaryContainerDark = Color(0xFFFFD9E2)
+
+val SecondaryDark = Color(0xFFEFBDCB)
+val OnSecondaryDark = Color(0xFF492532)
+val SecondaryContainerDark = Color(0xFF623B48)
+val OnSecondaryContainerDark = Color(0xFFFFD9E2)
+
+val TertiaryDark = Color(0xFFF5BA96)
+val OnTertiaryDark = Color(0xFF4C2609)
+val TertiaryContainerDark = Color(0xFF663B1D)
+val OnTertiaryContainerDark = Color(0xFFFFDBCA)
+
+val ErrorDark = Color(0xFFFFB4AB)
+val OnErrorDark = Color(0xFF690005)
+val ErrorContainerDark = Color(0xFF93000A)
+val OnErrorContainerDark = Color(0xFFFFDAD6)
+
+val BackgroundDark = Color(0xFF201A1B)
+val OnBackgroundDark = Color(0xFFECE0E1)
+val SurfaceDark = Color(0xFF201A1B)
+val OnSurfaceDark = Color(0xFFECE0E1)
+val SurfaceVariantDark = Color(0xFF514347)
+val OnSurfaceVariantDark = Color(0xFFD5C2C6)
